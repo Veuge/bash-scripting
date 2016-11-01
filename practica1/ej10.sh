2acfd240069990123385b07f5a48a063813358df
Veuge <nicky.0592@gmail.com>
2016-11-01 11:59:08 -0400

    ej10 fixed

--- practica1/ej10.sh
+++ practica1/ej10.sh
@@ -4,21 +4,24 @@
 #
 
 if [ $# -ne 2 ]; then
-    printf "Incorrecto!, para usar el script ./ej10.sh [nombre_directorio] [cadena]";
+    echo "Incorrecto!, para usar el script ./ej10.sh [nombre_directorio] [cadena]";
+    exit 1;
 else
     # Existe un directorio con el nombre proporcionado por el usuario?
     if [[ -d $1 ]]; then
 	echo Directorio con nombre $1 encontrado. Buscando ficheros...
-	FILES=$(ls -l $1/*$2* | cut -d" " -f1,9);
-	NUMBER="${FILES}" | wc -l;
-	echo "${NUMBER}"
+	NUMBER=$(ls -1  $1/*$2* 2> /dev/null | wc -l);
+	echo "Numero de ficheros que contienen en su nombre $2: ${NUMBER}"
 	if [ $NUMBER -eq 0 ]; then
 	    echo No se encontraron ficheros que contengan $2 en su nombre;
+	    exit 1;
 	else
 	    echo Ficheros en el directorio $1 con nombre que contiene la secuencia $2:
-	    echo $FILES
+	    echo -e "$(ls -l $1/*$2* | cut -d" " -f1,9)"
+	    exit 0;
 	fi
     else
         echo No existe ningun directorio con el nombre $1;
+	exit 1;
     fi
 fi
