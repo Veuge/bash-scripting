6b1c929e01638b152043ffa4217e013a6cf297ac
Veuge <nicky.0592@gmail.com>
2016-11-07 11:45:40 -0400

    changes

--- practica1/1.sh
+++ practica1/1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Crear un script que muestre el numero de ficheros que tienen permiso de lectura, escritura y
-# ejecucion para el grupo.
+# Crear un script que muestre el numero de ficheros que tienen permiso de lectura, escritura y ejecucion para el grupo.
 
 DIR=/home/veuge/
 NUMBER=`find $DIR -maxdepth 1 -type f -perm -g+rwx | wc -l`
